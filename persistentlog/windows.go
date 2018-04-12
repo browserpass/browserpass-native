a0a6932935c66e77f0571fdb2b5f0568629ca9ad
Maxim Baz <git@maximbaz.com>
2018-04-13 01:55:31 +0200

    Store logs in browserpass subfolder of %LOCALAPPDATA%

--- persistentlog/windows.go
+++ persistentlog/windows.go
@@ -12,12 +12,17 @@ import (
 
 // AddPersistentLogHook configures persisting logs in a file
 func AddPersistentLogHook() {
-	appDataPath := os.Getenv("APPDATA")
+	appDataPath := os.Getenv("LOCALAPPDATA")
 	if appDataPath == "" {
 		log.Warn("Unable to determine %%APPDATA%% folder location, logs will NOT be persisted")
 		return
 	}
-	logFilePath := filepath.Join(appDataPath, "browserpass.log")
+	logFolderPath := filepath.Join(appDataPath, "browserpass")
+	if err := os.MkdirAll(logFolderPath, os.ModePerm); err != nil {
+		log.Warn("Unable to create browserpass folder in %%APPDATA%%, logs will NOT be persisted")
+		return
+	}
+	logFilePath := filepath.Join(logFolderPath, "browserpass.log")
 	log.Debug("Logs will being written to: ", logFilePath)
 	log.AddHook(lfshook.NewHook(logFilePath, &log.TextFormatter{FullTimestamp: true}))
 }
